8dbc0039cf936a83a5a6aecb25d669cba4d96864
Alan Ward <arw180@gmail.com>
2014-08-23 15:36:12 -0400

    more updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
+var Moment = require('moment');
 
 function render(resume) {
   var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
@@ -22,20 +23,26 @@ function render(resume) {
 };
 
 function setTwitterUsername(resume) {
-    for (var i=0; i < resume.basics.profiles.length; i++) {
-    if (resume.basics.profiles[i].network.toLowerCase() === 'twitter') {
-      resume.basics.twitter = resume.basics.profiles[i].username;
-    }
+  for (var i=0; i < resume.basics.profiles.length; i++) {
+  if (resume.basics.profiles[i].network.toLowerCase() === 'twitter') {
+    resume.basics.twitter = resume.basics.profiles[i].username;
   }
+}
 }
 
 function setGithubUsername(resume) {
-    for (var i=0; i < resume.basics.profiles.length; i++) {
+  for (var i=0; i < resume.basics.profiles.length; i++) {
     if (resume.basics.profiles[i].network.toLowerCase() === 'github') {
       resume.basics.github = resume.basics.profiles[i].username;
     }
   }
 }
+
+Handlebars.registerHelper("prettifyDate", function(resumeDate) {
+  var newDate = Moment(resumeDate).format('MMM YYYY');
+  console.log('newDate: ' + newDate);
+  return newDate;
+});
 
 module.exports = {
 	render: render
