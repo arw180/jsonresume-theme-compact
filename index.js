17bb310edae0072d3fbdb7632279c3e0d80e24ee
Alan Ward <arw180@gmail.com>
2014-08-23 14:27:04 -0400

    header in good shape

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ var Handlebars = require('handlebars');
 function render(resume) {
   var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+
+  var skillGroup1 = [];
+  skillGroup1.push(resume.skills[0]);
+  skillGroup1.push(resume.skills[1]);
+  resume.skillsGroup1 = skillGroup1;
+  // TODO: skill group 2
+
+  setTwitterUsername(resume);
+  setGithubUsername(resume);
+
+
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
 	});
 };
+
+function setTwitterUsername(resume) {
+    for (var i=0; i < resume.basics.profiles.length; i++) {
+    if (resume.basics.profiles[i].network.toLowerCase() === 'twitter') {
+      resume.basics.twitter = resume.basics.profiles[i].username;
+    }
+  }
+}
+
+function setGithubUsername(resume) {
+    for (var i=0; i < resume.basics.profiles.length; i++) {
+    if (resume.basics.profiles[i].network.toLowerCase() === 'github') {
+      resume.basics.github = resume.basics.profiles[i].username;
+    }
+  }
+}
 
 module.exports = {
 	render: render
